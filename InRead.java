47141f20df249d88cd14ce6a925d3c719822aeec
Jeremy Hughes <jeremyvhughes@gmail.com>
2015-11-23 18:17:31 -0600

    Initial read/write files.

---
+++ InRead.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.System.*;
+
+class InRead{
+	public static void main(String[] args){
+		try{
+			FileReader fr = new FileReader("log.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			String str;
+			while((str = br.readLine()) != null){
+				System.out.println(str + "\n");
+			}
+			br.close();
+		}
+		catch(IOException e){
+			System.out.println("File not found.");
+		}
+	}
+}
---
+++ OutWrite.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import static java.lang.System.*;
+
+class OutWrite{
+	public static void main(String[] args){
+		try{
+			FileWriter fw = new FileWriter("log.txt");
+			PrintWriter pw = new PrintWriter(fw);
+			
+			pw.println("My");
+			pw.println("name");
+			pw.println("is");
+			pw.println("Jeremy.");
+			
+			pw.close();
+		}
+		catch(IOException e){
+			out.println("Error!");
+		}
+	}
+}
