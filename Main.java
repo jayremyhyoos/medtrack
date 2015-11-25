017a85122c4c3a95958849cb4b535dd55c5d8ea1
Jeremy Hughes <Jeremy Hughes>
2015-11-25 17:14:41 -0600

    shit's fucked cap'n

---
+++ Main.java
@@ -0,0 +1,15 @@
+package medtrack;
+
+import java.util.Scanner;
+import java.io.*;
+import java.lang.System.*;
+
+class Main{
+	public static void main(String[] args){
+		Scanner stdin = new Scanner(System.in);
+		
+		
+		System.out.println("To confirm that you've taken your daily prescription, ");
+		System.out.println("enter a '1'. To view the log book, enter a '2'");
+	}
+}
