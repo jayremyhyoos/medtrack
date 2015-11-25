a48089d796f0483c6750c6ac68def088bf7b3c23
Jeremy Hughes <jeremyvhughes@gmail.com>
2015-11-25 12:26:12 -0600

    Printing now works; Now to find out how to print on new lines without overwriting files.

--- MainMenu.java
+++ MainMenu.java
@@ -5,20 +5,28 @@ import java.lang.System.*;
 class MainMenu{
 	public static void main(String[] args){
 		Scanner stdin = new Scanner(System.in);
-		System.out.print("Good morning! Have you taken your medicine? (Y/N) ");
-		String answer = stdin.next().toLowerCase();
-		if(answer == "y"){
+		boolean varOne;
+		String answer;
+		
+		System.out.print("Have you taken your medicine? (Y/N) ");
+		answer = stdin.next().toLowerCase();
+		System.out.println("You entered: " + answer);
+		
+		if (answer.equals("y")){
 			try{
 			FileWriter fw = new FileWriter("medicinelog.txt");
 			PrintWriter pw = new PrintWriter(fw);
 			
-			pw.println("11/24/15 -------------------- Taken");
+			pw.println("11/25/15 ---------------------------------------------------------- Taken");
 			
 			pw.close();
 			}
 			catch(IOException e){
 				System.out.println("Error!");
 			}
+		}
+		else{
+			
 		}
 	}
 }
