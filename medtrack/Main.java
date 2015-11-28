a5d1eb8f1c5adf62be7de176fdbb59983314d762
Jorrd <jordanray93@hotmail.co.uk>
2015-11-28 14:27:07 +0000

    Updating Main.java
    Added in a few changes - there's most likely a lot more updates you can make but I loved this for a first project for you.

--- medtrack/Main.java
+++ medtrack/Main.java
@@ -2,7 +2,6 @@ package medtrack;
 
 import java.util.Scanner;
 import java.io.*;
-import java.lang.System.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
@@ -10,6 +9,7 @@ class Main{
 	public static void main(String[] args){
 		Scanner stdin = new Scanner(System.in);
 		int userChoice;
+		boolean taken = false;
 		Date today = new Date();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		String date = dateFormat.format(today);
@@ -20,31 +20,58 @@ class Main{
 		
 		if (userChoice == 1){
 			try{
-			FileWriter fw = new FileWriter("medtrack/medicinelog.txt", true);
-			PrintWriter pw = new PrintWriter(fw);
-			
-			pw.println(date + " ---------------------------------------------------------------------------------------- Taken\n");
-			
-			pw.close();
-			}
-			catch(IOException e){
-				System.out.println("Error!");
+				FileWriter fw = new FileWriter("medicinelog.txt", true);
+				FileReader fr = new FileReader("medicinelog.txt");
+				BufferedReader br = new BufferedReader(fr);
+				
+				String str;
+				while((str = br.readLine()) != null) {
+					if (str.contains(date)){
+						taken = true;
+					}
+				}
+				
+				if (taken){
+					System.out.println("You have already entered your report for today.");
+				}else{
+					PrintWriter pw = new PrintWriter(fw);
+					
+					pw.println("Meds were taken on " + date);
+					
+					pw.close();
+					stdin.close();
+					System.out.println("Logging complete for " + date);
+					}
+				}
+			catch(IOException e) {
+				System.out.println("Exception thrown " + e);
 			}
 		}
-		else if (userChoice == 2){
+		else if (userChoice == 2) {
 			try{
-			FileReader fr = new FileReader("medtrack/medicinelog.txt");
-			BufferedReader br = new BufferedReader(fr);
-			
-			String str;
-			while((str = br.readLine()) != null){
-				System.out.println(str + "\n");
+				FileReader fr = new FileReader("medicinelog.txt");
+				BufferedReader br = new BufferedReader(fr);
+				
+				String str;
+				while((str = br.readLine()) != null) {
+					System.out.println(str + "\n");
+					if (str.contains(date)){
+						taken = true;
+					}
+				}
+				br.close();
+				System.out.println("Todays date is: " + date);
+				if (taken)
+					System.out.println("You have already taken your meds today.");
+				else
+					System.out.println("You have not yet taken your meds today - TAKE THEM.");
+				stdin.close();
+				}
+			catch(IOException e) {
+				System.out.println("IOException thrown: " + e);
 			}
-			br.close();
-			}
-			catch(IOException e){
-				System.out.println("File not found.");
-			}
-		}		
+		} else {
+			System.out.println("You must enter either 1 or 2");
+		}
 	}
 }
